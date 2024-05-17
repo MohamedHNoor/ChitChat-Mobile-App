f1ca05c065d11f12f0a290103ba31d0fd960b76a
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-05-17 16:01:31 +0200

    Update the key names

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,19 +4,21 @@ import { getFirestore, collection } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_NATIVE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_NATIVE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_NATIVE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_NATIVE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_NATIVE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_NATIVE_APP_FIREBASE_APP_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
+
+console.log('apiKey', process.env.apiKey);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // authentication variable
-export const auth = initializeAuth(app, {
+export const Auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 
